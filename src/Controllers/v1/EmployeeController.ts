8763969c99cbb8a80a90a241bd8ab880e1a7e3cc
Prabhdeep Singh Multani <ps.multani05@gmail.com>
2023-02-02 15:56:46 +0100

    POST employee with task

--- src/Controllers/v1/EmployeeController.ts
+++ src/Controllers/v1/EmployeeController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 import ControllerDecorator from '@app/Decorators/ControllerDecorator'
 import EmployeeService from '@app/Services/EmployeeService'
+import TaskService from '@app/Services/TaskService'
 import Task from '@app/Models/Task'
 
 @ControllerDecorator
@@ -16,7 +17,32 @@ class EmployeeController {
     const employee = await EmployeeService.store(payload)
     response.json({ data: employee })
   }
+/**
+ * 
+ * @param request : Object
+ * @param response : Object
+ * 
+ * it takes the employee data and the new associated task to save
+ * them in their respective tables with the necessary associations
+ * and returns to the client as response a copy of the task and its 
+ * owner
+ */
+  public async storeWithTask(request: Request, response: Response) {
+    const { employee: employeeData, task: taskData } = request.body
 
+    const employee = await EmployeeService.store(employeeData)
+    const task = await TaskService.store({
+      ...taskData,
+      employee_id: employee.employee_id_number
+    })
+
+    response.json({ data: {
+      ...employee,
+      task: {
+        ...task,
+      }
+    } })
+  }
   public async show(request: Request, response: Response) {
     const { id } = request.params
     const employee = await EmployeeService.findOrFail(id)
--- src/Routes/Api/V1/employees.ts
+++ src/Routes/Api/V1/employees.ts
@@ -7,12 +7,12 @@ export default () => {
 
   employees.get('/', EmployeeController.index)
   employees.post('/', EmployeeController.store)
+  employees.post('/task', EmployeeController.storeWithTask)
   employees.get('/tasks', EmployeeController.employeesWithtasks)
   employees.get('/:id', EmployeeController.show)
   employees.put('/:id', EmployeeController.update)
   employees.patch('/:id', EmployeeController.update)
   employees.delete('/:id', EmployeeController.delete)
-
   employees.get('/:id/tasks', EmployeeController.tasks)
 
   return employees
